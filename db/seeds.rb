f80bf751b7123dab6c80143b0ce78f19c846f0b6
Skyler Torian <skyler.torian@yahoo.com>
2020-06-17 12:56:21 -0400

    set up test db to validate associations'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Company.destroy_all
+User.destroy_all
+UserCompany.destroy_all
+
+apple = Company.create(symbol: "AAPL", company: "Apple Inc", name: "Apple", exchange: "Nasdaq", industry: "Computer Hardware", website: "www.apple.com", description: "Apple Inc is an American multinational technology company. It designs, manufactures, and markets mobile communication and media devices, personal computers, and portable digital music players", ceo: "Tim Cook", sector: "Technology")
+ge = Company.create(symbol: "GE", company: "General Electric Co", name: "GE", exchange: "Nasdaq", industry: "Hardware", website: "www.ge.com", description: "General Electric is an American multinational technology company. It makes microwaves, ovens, planes, power plants, and more", ceo: "Billy Eisner", sector: "Technology")
+
+skyler = User.create(username: "skyler")
+brook = User.create(username: "Brook")
+
+stocks1 = UserCompany.create(user_id: 1, company_id: 1)
+stocks2 = UserCompany.create(user_id: 2, company_id: 2)
+stocks3 = UserCompany.create(user_id: 1, company_id: 2)
