e0bad0c04b8f7d9c22019a234de2332def223d95
Skyler Torian <skyler.torian@yahoo.com>
2020-06-18 15:49:14 -0400

    added code for sign up and validation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,28 @@
 class SessionsController < ApplicationController
+  def new
+   
+  end
+
   def create
+    @user = User.find_by(username: params[:username])
+    if @user
+
+      session[:user_id] = @user.id 
+      redirect_to user_path(@user)
+    else
+    
+      flash.notice = "No user found"
+      render :new
+    end
   end
 
   def destroy
+    session.clear
+    redirect_to login_path
   end
 
-  def new
+  def home
+    redirect_to login_path
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -13,6 +17,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     @user.password = params[:password]
     @user.save!
+    redirect_to login_path
   end
 
   def login
@@ -25,7 +30,15 @@ class UsersController < ApplicationController
   end
 
 
+private
 
+def find_user
+  @user = User.find(params[:id])
+end
+
+def user_params
+  params.require(:user).permit!
+end
 
   
 end
